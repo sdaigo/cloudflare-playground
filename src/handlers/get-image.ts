76c95e4383a364b1d64102c50dea31e901c6d008
Daigo Shitara <shitara@tagbangers.co.jp>
2024-05-25 05:38:41 +0900

    retrieve list, single

--- src/handlers/get-image.ts
+++ src/handlers/get-image.ts
@@ -1,16 +1,44 @@
 import type { IRequest } from "itty-router";
-import { ALL_IMAGES } from "../data/image_store";
+import { ResultAsync } from "neverthrow";
 
-export default function getImage(request: IRequest) {
-  const image = ALL_IMAGES.find(image => image.id === Number.parseInt(request.params.id));
+export default function getImage(request: IRequest, env: Env) {
+  const stmt = env.DB.prepare(`
+    SELECT i.*, c.display_name AS category_display_name
+    FROM images i
+    INNER JOIN image_categories c ON i.category_id = c.id
+    WHERE i.id = ?1`).bind(request.params.id);
 
-  if (!image) {
-    return new Response("Not Found", { status: 404 });
-  }
-
-  return new Response(JSON.stringify(image), {
-    headers: {
-      "content-type": "application/json",
+  const result = ResultAsync.fromPromise(
+    stmt.first(), //
+    () => {
+      return { message: "No images found" };
     },
-  });
+  );
+
+  return result.match(
+    image => {
+      if (!image) {
+        return new Response(JSON.stringify({ message: `Not Found: id ${request.params.id}` }), {
+          status: 404,
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+      }
+
+      return new Response(JSON.stringify(image), {
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+    },
+    error => {
+      return new Response(JSON.stringify(error), {
+        status: 500,
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+    },
+  );
 }
